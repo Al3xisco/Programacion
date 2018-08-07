3371a9b794693303832ce6c2c42c396b64acee86
Alexis Carrión <33529522+Al3xisco@users.noreply.github.com>
2018-08-06 22:46:22 -0500

    Crud

--- CRUD_EstacionamientoBicis/src/controllers/controles.js
+++ CRUD_EstacionamientoBicis/src/controllers/controles.js
@@ -1,4 +1,4 @@
-
+ 
 const controller = {
   listar:(req,res)=>{
     req.getConnection((error,conexion)=>{
--- CRUD_EstacionamientoBicis/src/routes/rutas.js
+++ CRUD_EstacionamientoBicis/src/routes/rutas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router=express.Router();
 const controles= require('../controllers/controles');
-
+ 
 
 router.get('/', controles.listar);
 router.post('/Insertar', controles.insertar);
